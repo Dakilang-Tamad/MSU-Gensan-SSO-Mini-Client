e2398265bcd30cd4188af94bb222a6f9267f3999
Dakilang-Tamad <59394084+Dakilang-Tamad@users.noreply.github.com>
2021-02-23 13:45:27 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,147 @@
+import tkinter as tk
+from tkinter import*
+from tkinter.ttk import*
+import tkinter.messagebox
+import urllib.request
+import webbrowser
+
+def open_url(url):
+    link = url
+    try:
+        browser = webbrowser.get('chrome')
+        browser.open_new(link)
+    except:
+        webbrowser.open_new(link)
+
+
+def connect(host='http://google.com'):
+    tkinter.messagebox.showinfo("Loading...", "Please wait while trying to connect to host site.")
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
+
+class MiniClient(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(S1)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+
+class S1(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.MSU = PhotoImage(file=r"resources\MSU_logo.png")
+        self.logo = self.MSU.subsample(2, 2)
+
+        self.BG = PhotoImage(file=r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master, image=self.BG)
+        self.bg.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.label2 = Label(master, text="Logo", bg=None, image=self.logo, justify=CENTER)
+        self.label2.place(x=340, y=100)
+
+        self.label = Label(master, text="Log in with Google:", bg=None, justify=CENTER)
+        self.label.place(x=400 ,y=475)
+
+        self.photo = PhotoImage(file=r"resources\gg_logo.png")
+        self.image = self.photo.subsample(2, 2)
+
+        self.login_button = Button(master, text="Log in with Google", width=50, image=self.image, command=lambda: master.switch_frame(S2) if connect() else self.login_error())
+        self.login_button.place(x=400, y=500)
+
+
+    def login_error(self):
+            tkinter.messagebox.showinfo("Warning", "Cannot connect to host site. Please check your internet connection and try again.")
+
+
+class S2(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.BG = PhotoImage(file = r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master,image = self.BG)
+        self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
+
+        self.grades = Button(master, text="GRADES",command=lambda: master.switch_frame(S3))
+        self.grades.place(x=250, y=15, height=125, width=500)
+
+        self.inc = Button(master, text="INC Grade Monitor", command=lambda: master.switch_frame(S4))
+        self.inc.place(x=250, y=160, height=125, width=500)
+
+        self.cor = Button(master, text="Certificate of Registration", command=lambda: master.switch_frame(S5))
+        self.cor.place(x=250, y=310, height=125, width=500)
+
+        self.links = Button(master, text="Other Links", command=lambda: master.switch_frame(S6))
+        self.links.place(x=250, y=460, height=125, width=500)
+
+
+class S3(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.BG = PhotoImage(file = r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master,image = self.BG)
+        self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
+
+        self.menu = Button(master, text="Back to Main Menu", command=lambda: master.switch_frame(S2))
+        self.menu.place(x=850, y=550, height=50, width=150)
+
+
+class S4(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.BG = PhotoImage(file = r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master,image = self.BG)
+        self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
+
+        self.menu = Button(master, text="Back to Main Menu", command=lambda: master.switch_frame(S2))
+        self.menu.place(x=850, y=550, height=50, width=150)
+
+class S5(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.BG = PhotoImage(file = r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master,image = self.BG)
+        self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
+
+        self.menu = Button(master, text="Back to Main Menu", command=lambda: master.switch_frame(S2))
+        self.menu.place(x=850, y=550, height=50, width=150)
+
+class S6(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.BG = PhotoImage(file = r"resources\BG.png")
+        self.BG = self.BG.zoom(1)
+        self.bg = Label(master,image = self.BG)
+        self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
+
+        self.menu = Button(master, text="Back to Main Menu", command=lambda: master.switch_frame(S2))
+        self.menu.place(x=850, y=550, height=50, width=150)
+
+        self.vle = Button(master, text="Virtual Learning Environment", command=lambda: open_url("vle.msugensan.edu.ph"))
+        self.vle.place(x=250, y=15, height=125, width=500)
+
+        self.elib = Button(master, text="E-Library", command=lambda: open_url("http://msulib.msugensan.edu.ph"))
+        self.elib.place(x=250, y=160, height=125, width=500)
+
+if __name__ == "__main__":
+    app = MiniClient()
+    app.title("MSU Student Portal Mini Client")
+    app.geometry("1000x600")
+    app.resizable(False, False)
+    icon = PhotoImage(file=r"resources\MSU_logo.png")
+    app.iconphoto(False, icon)
+    app.mainloop()
+
