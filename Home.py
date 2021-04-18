6b917245f7579011fda8b9db68412cbc45117994
Dakilang-Tamad <59394084+Dakilang-Tamad@users.noreply.github.com>
2021-04-18 13:31:04 +0800

    Add files via upload

--- Home.py
+++ Home.py
@@ -5,7 +5,11 @@ from tkinter.ttk import*
 import tkinter.messagebox
 import urllib.request
 import webbrowser
-from login_and_scrape import login
+from selenium import webdriver
+from time import *
+import ctypes
+from win10toast import ToastNotifier
+from selenium.webdriver.chrome.options import Options
 
 
 def open_url(url):
@@ -32,6 +36,124 @@ def connect(host='https://sso.msugensan.edu.ph'):
         return True
     except:
         return False
+
+def login(mail, word):
+    try:
+
+        popup = ToastNotifier()
+
+        ctypes.windll.user32.MessageBoxW(0,"The login sequence will now start. Please wait until the main window changes.", "Warning", 0)
+        ctypes.windll.user32.MessageBoxW(0,"A terminal window will appear for the login sequence. Please do not close that window until the sequence is over i.e. the main window changes screens or an error occurs.", "WARNING", 1)
+
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--gpuless")
+
+        driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+        driver.get('https://sso.msugensan.edu.ph')
+        main_window = driver.current_window_handle
+
+        sleep(5)
+
+        popup.show_toast("Login process", "Opening host site", duration=3)
+
+        grade_button = driver.find_element_by_class_name('abcRioButtonContentWrapper')
+        grade_button.click()
+
+        sleep(5)
+
+        for handle in driver.window_handles:
+            if handle != main_window:
+                login_page = handle
+
+        driver.switch_to.window(login_page)
+
+        popup.show_toast("Login process", "Entering email", duration=3)
+
+        e_mail = mail
+
+        try:
+            email = driver.find_element_by_id('identifierId')
+            email.send_keys(e_mail)
+            next = driver.find_element_by_xpath('//div[@id="identifierNext"]')
+            next.click()
+        except:
+            email = driver.find_element_by_id('Email')
+            email.send_keys(e_mail)
+            next = driver.find_element_by_xpath('//input[@id="next"]')
+            next.click()
+
+        sleep(5)
+
+        popup.show_toast("Login process", "Entering password", duration=3)
+
+        pass_word = word
+
+        try:
+            password = driver.find_element_by_xpath('//input[@jsname="YPqjbf"]')
+            password.send_keys(pass_word)
+            next = driver.find_element_by_xpath('//div[@jsname="Njthtb"]')
+            next.click()
+        except:
+            password = driver.find_element_by_id('password')
+            password.send_keys(pass_word)
+            next = driver.find_element_by_xpath('//input[@id="submit"]')
+            next.click()
+
+        sleep(10)
+
+        driver.switch_to.window(main_window)
+
+        popup.show_toast("Scraping process", "Accessing grades", duration=3)
+
+        sleep(10)
+
+        grade_button = driver.find_element_by_xpath('//a[@id="view_grade"]')
+        grade_button.click()
+
+        sleep(5)
+
+        popup.show_toast("Scraping process", "Scraping grades", duration=3)
+
+        subcodes = driver.find_elements_by_xpath('//td[@data-label="Subject code"]')
+        sections = driver.find_elements_by_xpath('//td[@data-label="Section"]')
+        grades = driver.find_elements_by_xpath('//td[@data-label="Grade"]')
+        descriptions = driver.find_elements_by_xpath('//td[@data-label="Description"]')
+        subcodes2 = []
+        sections2 = []
+        grades2 = []
+        descriptions2 = []
+
+        for i in subcodes:
+            subcodes2.append(i.text)
+        for i in sections:
+            sections2.append(i.text)
+        for i in grades:
+            grades2.append(i.text)
+        for i in descriptions:
+            descriptions2.append(i.text)
+
+        sub_file = open("user_files\suject_codes.txt", "w")
+        sec_file = open("user_files\section_codes.txt", "w")
+        gr_file = open("user_files\grades.txt", "w")
+        des = open("user_files\descriptions.txt", "w")
+
+        for i in subcodes2:
+            sub_file.writelines(i + "\n")
+        for i in sections2:
+            sec_file.writelines(i + "\n")
+        for i in grades2:
+            gr_file.writelines(i + "\n")
+        for i in descriptions2:
+            des.writelines(i + "\n")
+
+        driver.close()
+
+        return True
+
+    except:
+        return False
+
 
 class MiniClient(tk.Tk):
     def __init__(self):
@@ -83,7 +205,9 @@ class S0(tk.Frame):
             if login(email, password):
                 MiniClient.switch_frame(master,S2)
             else:
-                tkinter.messagebox.showinfo("Error", "Cannot login to Student Portal. This might be due to a connection problem or something else.")
+                tkinter.messagebox.showwarning("Error", "Cannot login to Student Portal. This might be due to a connection problem or the site is currently unavailable.")
+        else:
+            MiniClient.switch_frame(master, S1)
 
 class S1(tk.Frame):
     def __init__(self, master):
@@ -92,6 +216,34 @@ class S1(tk.Frame):
         self.BG = self.BG.zoom(1)
         self.bg = Label(master, image=self.BG)
         self.bg.place(x=0, y=0, relwidth=1, relheight=1)
+        tkinter.messagebox.showinfo("Note", "Input data will not be permanently stored without user permission.")
+        self.e_tag = tk.Entry(master)
+        self.p_tag = tk.Entry(master, show="*")
+        self.mail = Label(master, text="Email:", bg=None, justify=CENTER)
+        self.word = Label(master, text="Password:", bg=None, justify=CENTER)
+        self.e_tag.place(x=430, y=250)
+        self.p_tag.place(x=430, y=300)
+        self.mail.place(x=430, y=230)
+        self.word.place(x=430, y=280)
+        self.enter = Button(master, text="Log in", command=lambda: self.login_function(master))
+        self.enter.place(x=453, y=330)
+
+    def login_function(self, master):
+        email = self.e_tag.get()
+        password = self.p_tag.get()
+        response = tkinter.messagebox.askyesno("Confirm", "Do you want to store your credentials for future auto login?")
+        if response:
+            file1 = open('user_files\login_data\email.txt', "w")
+            file2 = open('user_files\login_data\password.txt', "w")
+            file1.write(email)
+            file2.write(password)
+            tkinter.messagebox.showinfo("Alert", "Your credentials were stored at user_file\login_data of the local directory")
+        if login(email, password):
+            MiniClient.switch_frame(master, S2)
+        else:
+            tkinter.messagebox.showwarning("Error","Cannot login to Student Portal. This might be due to a connection problem or something else.")
+            MiniClient.switch_frame(master, S0)
+
 
 
 class S2(tk.Frame):
@@ -123,8 +275,49 @@ class S3(tk.Frame):
         self.bg = Label(master,image = self.BG)
         self.bg.place(x=0,y=0,relwidth = 1, relheight = 1)
 
+        self.buttons = []
+        self.subCodes = []
+        self.grades = []
+        self.secs = []
+        self.desc = []
+
+        self.sub_file = open("user_files\suject_codes.txt","r")
+        self.sec_file = open("user_files\section_codes.txt", "r")
+        self.gr_file = open("user_files\grades.txt", "r")
+        self.desc_file = open("user_files\descriptions.txt", "r")
+
+        self.tst = self.sub_file.readline()
+        while self.tst != '':
+            self.subCodes.append(self.tst)
+            self.grades.append(self.gr_file.readline())
+            self.secs.append(self.sec_file.readline())
+            self.desc.append(self.desc_file.readline())
+            self.tst = self.sub_file.readline()
+
+        self.max = len(self.subCodes)
+        self.x = 0
+        self.y = 15
+        for i in range(self.max):
+            self.button = Button(master, text = self.subCodes[i], command=lambda a=i: self.show_grade(a))
+            self.check1 = i % 10
+            if i == 0:
+                self.x = 15
+            else:
+                self.x = self.x + 100
+            if self.check1 == 0:
+                self.x = 15
+                self.y = self.y + 60
+            self.button.place(x=self.x, y=self.y)
+            self.buttons.append(self.button)
+
         self.menu = Button(master, text="Back to Main Menu", command=lambda: master.switch_frame(S2))
         self.menu.place(x=850, y=550, height=50, width=150)
+
+    def show_grade(self, index):
+        tkinter.messagebox.showinfo("Grade Summary",
+                                    "Description: " + self.desc[index] +
+                                    "\n Section: " + self.secs[index] +
+                                    "\n Grade: " + self.grades[index])
 
 
 class S4(tk.Frame):
